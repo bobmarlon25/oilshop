6818ead498388313785eff020d722d20d072fda4
bobmarlon25 <julianpuentes25@gmail.com>
2024-04-01 19:00:42 -0500

    modulo_1 listo

--- oilshop/settings.py
+++ oilshop/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'web',
+    'django_admin_tailwind',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,13 +75,22 @@ WSGI_APPLICATION = 'oilshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dboil',
+        'USER': 'postgres',
+        'PASSWORD': 'mjpp3142159888',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -123,3 +134,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import  os
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Categoria,Producto
 # Register your models here.
+admin.site.register(Categoria)
+# admin.site.register(Producto)
+
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display=('nombre','precio','categoria') 
+    list_editable=('precio',)
