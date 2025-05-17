d20c1fc758c79b8d5a27714aa8e6144f93f936f6
julian <julianpuentes25@gmail.com>
2025-05-16 22:50:05 -0500

    link_home

--- oilshop/settings.py
+++ oilshop/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ha8jkxc_z&opn@#w%!(&k8q(=k%5z*!*57s=dg-rqd#h*tazg)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =False
 
 ALLOWED_HOSTS = ['*']
 
